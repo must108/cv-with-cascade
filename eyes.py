02d3df5cfc171615e081cf15a6999df1ece1de44
mustaeen <136145181+must108@users.noreply.github.com>
2024-03-23 16:53:17 -0400

    first commit

---
+++ eyes.py
@@ -0,0 +1,17 @@
+import cv2
+
+imageFile = input('Enter image file name: ')
+filename = 'images/' + imageFile
+
+image = cv2.imread(filename)
+rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+detector = cv2.CascadeClassifier('data/haarcascade_eye.xml')
+rects = detector.detectMultiScale(rgb, scaleFactor=1.3, minNeighbors= 2, minSize=(75, 75))
+
+for(i, (x, y, w, h)) in enumerate(rects):
+    cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
+    cv2.putText(image, "Eye #{}".format(i + 1), (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.55, (0, 0, 255), 2)
+
+cv2.imshow('Eyes', image)
+cv2.waitKey(0)
