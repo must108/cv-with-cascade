fb2c00d202465c5a879442eac6e255d9e67c15f2
must108 <136145181+must108@users.noreply.github.com>
2024-03-26 09:50:07 -0400

    add faces functionality

--- cats.py
+++ cats.py
@@ -1,7 +1,7 @@
 import cv2
 
 imageFile = input('Enter image file name: ')
-filename = 'images/' + imageFile
+filename = 'assets/' + imageFile
 
 image = cv2.imread(filename)
 rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
@@ -11,7 +11,7 @@ rects = detector.detectMultiScale(rgb, scaleFactor=1.3, minNeighbors= 1, minSize
 
 for (i, (x, y, w, h)) in enumerate(rects):
     cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
-    cv2.putText(image, "Cat #{}".format(i + 1), (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.55, (0, 0, 255), 2)
+    cv2.putText(image, "Cat Face #{}".format(i + 1), (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.55, (0, 0, 255), 2)
 
 cv2.imshow('Cat Faces', image)
 cv2.waitKey(0)
