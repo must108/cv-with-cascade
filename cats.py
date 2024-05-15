3a28bdd8b8f6aa3893386b7e185a1754683dc092
mustaeen <136145181+must108@users.noreply.github.com>
2024-05-15 02:58:12 -0400

    color edits and readme stuff

--- cats.py
+++ cats.py
@@ -10,8 +10,8 @@ detector = cv2.CascadeClassifier('data/haarcascade_frontalcatface_extended.xml')
 rects = detector.detectMultiScale(rgb, scaleFactor=1.3, minNeighbors= 1, minSize=(75, 75))
 
 for (i, (x, y, w, h)) in enumerate(rects):
-    cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
-    cv2.putText(image, "Cat Face #{}".format(i + 1), (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.55, (0, 0, 255), 2)
+    cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    cv2.putText(image, "Cat Face #{}".format(i + 1), (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.55, (0, 255, 0), 2)
 
 cv2.namedWindow('Cat Faces', cv2.WINDOW_NORMAL)
 cv2.imshow('Cat Faces', image)
--- eyes.py
+++ eyes.py
@@ -10,8 +10,8 @@ detector = cv2.CascadeClassifier('data/haarcascade_eye.xml')
 rects = detector.detectMultiScale(rgb, scaleFactor=1.3, minNeighbors= 2, minSize=(75, 75))
 
 for(i, (x, y, w, h)) in enumerate(rects):
-    cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
-    cv2.putText(image, "Eye #{}".format(i + 1), (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.55, (0, 0, 255), 2)
+    cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    cv2.putText(image, "Eye #{}".format(i + 1), (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.55, (0, 255, 0), 2)
 
 cv2.namedWindow('Eyes', cv2.WINDOW_NORMAL)
 cv2.imshow('Eyes', image)
